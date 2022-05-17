55d35c61b64404ba38b159e409c1331fba1c5f38
kukina622 <wyes91622@gmail.com>
2022-05-18 04:57:40 +0800

    complete show result url and copy to clip

--- frontend/src/components/ShortenResult.tsx
+++ frontend/src/components/ShortenResult.tsx
@@ -1,14 +1,39 @@
 import { Box, Button, Text } from "@chakra-ui/react";
+import Swal from "sweetalert2";
 
-function ShortenResult() {
+interface shortenResultprop {
+  shortenUrl: string;
+}
+
+function ShortenResult({ shortenUrl }: shortenResultprop) {
+  function copyToclip() {
+    if (!navigator.clipboard) return;
+    navigator.clipboard.writeText(shortenUrl).then(() => {
+      Swal.fire({
+        icon: "success",
+        text: '複製成功',
+        showConfirmButton: false,
+        timer: 800
+      });
+    });
+  }
   return (
     <Box mb={5}>
-      <Text fontSize="2rem" textAlign={"center"} mb={2}>
-        https://elearning.yuntech.edu.tw/sys/reg/ssoLogin.php
-      </Text>
-      <Button m="0 auto" colorScheme="purple" display="block">
-        複製
-      </Button>
+      {shortenUrl === "" || (
+        <>
+          <Text fontSize="2rem" textAlign={"center"} mb={2}>
+            {shortenUrl}
+          </Text>
+          <Button
+            m="0 auto"
+            colorScheme="purple"
+            display="block"
+            onClick={() => copyToclip()}
+          >
+            複製
+          </Button>
+        </>
+      )}
     </Box>
   );
 }

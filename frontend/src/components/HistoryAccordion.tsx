4eb39afbe24640551f87154c6a1e6b209ce07251
kukina622 <wyes91622@gmail.com>
2022-05-18 05:31:34 +0800

    complete show history url

--- frontend/src/components/HistoryAccordion.tsx
+++ frontend/src/components/HistoryAccordion.tsx
@@ -6,39 +6,54 @@ import {
   AccordionIcon,
   Box
 } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { urlInfoProp } from "@/types/url";
 
-function HistoryAccordion() {
+interface historyAccordionprop {
+  urlInfo: urlInfoProp;
+}
+
+function HistoryAccordion({ urlInfo }: historyAccordionprop) {
+  const [shortenHistory, setShortenHistory] = useState<[urlInfoProp] | []>([]);
+
+  useEffect(() => {
+    const check = localStorage.getItem("shortenHistory");
+    if (check === null) {
+      localStorage.setItem("shortenHistory", "[]");
+    }
+    const _shortenHistory = JSON.parse(
+      localStorage.getItem("shortenHistory") as string
+    );
+    setShortenHistory(_shortenHistory);
+    if (urlInfo.originUrl === "" || urlInfo.shortenUrl === "") return;
+    localStorage.removeItem("shortenHistory");
+    setShortenHistory((prev) => {
+      const next = [...prev];
+      next.unshift(urlInfo);
+      localStorage.setItem("shortenHistory", JSON.stringify(next));
+      return next as [urlInfoProp] | [];
+    });
+  }, [urlInfo.originUrl]);
+
   return (
     <Accordion defaultIndex={[0]} allowMultiple>
-      <AccordionItem>
-        <AccordionButton borderWidth="1px" borderRadius="lg">
-          <Box flex="1" textAlign="left">
-            Section 1 title
-          </Box>
-          <AccordionIcon />
-        </AccordionButton>
-        <AccordionPanel pb={4}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </AccordionPanel>
-      </AccordionItem>
-
-      <AccordionItem>
-        <AccordionButton borderWidth="1px" borderRadius="lg">
-          <Box flex="1" textAlign="left">
-            Section 2 title
-          </Box>
-          <AccordionIcon />
-        </AccordionButton>
-        <AccordionPanel pb={4}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </AccordionPanel>
-      </AccordionItem>
+      {shortenHistory.map(({ originUrl, shortenUrl }: urlInfoProp, index) => (
+        <AccordionItem key={index}>
+          <AccordionButton
+            borderWidth="1px"
+            borderRadius="lg"
+            bg="#805ad5"
+            color="white"
+            _hover={{ color: "white", bg : "#6b46c1" }}
+          >
+            <Box flex="1" textAlign="left">
+              {shortenUrl}
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>{originUrl}</AccordionPanel>
+        </AccordionItem>
+      ))}
     </Accordion>
   );
 }

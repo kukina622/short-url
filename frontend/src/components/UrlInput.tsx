0624656983cab1b5e883303b097b6e22a7e62829
kukina622 <wyes91622@gmail.com>
2022-05-18 04:49:57 +0800

    complete send url to backend in UrlInput

--- frontend/src/components/UrlInput.tsx
+++ frontend/src/components/UrlInput.tsx
@@ -4,24 +4,63 @@ import {
   Flex,
   FormControl,
   FormLabel,
-  Input,
+  Input
 } from "@chakra-ui/react";
+import { urlInfoProp } from "@/types/url";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { apiPostUrl } from "@/api/url";
+import Swal from "sweetalert2";
 
-function UrlInput() {
+interface urlInputprop {
+  setUrlInfo: React.Dispatch<React.SetStateAction<urlInfoProp>>;
+}
+
+interface IFormInputs {
+  url?: string;
+}
+
+function UrlInput({ setUrlInfo }: urlInputprop) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit: SubmitHandler<IFormInputs> = async ({ url }, e) => {
+    e?.preventDefault();
+    try {
+      const res = await apiPostUrl(url as string);
+      const shortenUrl = `http://127.0.0.1:8080/${res.data.mapText}` || "";
+      setUrlInfo({ originUrl: url as string, shortenUrl });
+      Swal.fire({
+        icon: "success"
+      });
+      e?.target.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Flex p={5} borderWidth="1px" borderRadius="lg" mb={5}>
-      <Box flex={10} mr={5}>
-        <FormControl>
-          <FormLabel htmlFor="url">URL Link</FormLabel>
-          <Input id="url" name="url" placeholder="Enter URL" />
-        </FormControl>
-      </Box>
-      <Box>
-        <Button mt={8} colorScheme="purple">
-          縮網址
-        </Button>
-      </Box>
-    </Flex>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Flex p={5} borderWidth="1px" borderRadius="lg" mb={5}>
+        <Box flex={10} mr={5}>
+          <FormControl>
+            <FormLabel htmlFor="url">URL Link</FormLabel>
+            <Input
+              id="url"
+              placeholder="Enter URL"
+              {...register("url", { required: true })}
+            />
+          </FormControl>
+        </Box>
+        <Box>
+          <Button mt={8} colorScheme="purple" type="submit">
+            縮網址
+          </Button>
+        </Box>
+      </Flex>
+    </form>
   );
 }
 

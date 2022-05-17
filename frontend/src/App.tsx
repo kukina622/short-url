c4dc59fe5dba32b42a7fe074fe58b924aedb146a
kukina622 <wyes91622@gmail.com>
2022-05-18 05:46:28 +0800

    complete ShortenResult UrlInput HistoryAccordion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
+import { Box } from "@chakra-ui/react";
+import UrlInput from "@/components/UrlInput";
+import ShortenResult from "@/components/ShortenResult";
+import HistoryAccordion from "@/components/HistoryAccordion";
+import { useState } from "react";
+import { urlInfoProp } from "@/types/url";
+
 function App() {
-  return <div className="App"></div>;
+  const [urlInfo, setUrlInfo] = useState<urlInfoProp>({
+    originUrl: "",
+    shortenUrl: ""
+  });
+  return (
+    <div className="App">
+      <Box m="0 auto" w="80%" h="100vh" minH="40rem" maxW="1920px">
+        <ShortenResult shortenUrl={urlInfo.shortenUrl} />
+        <UrlInput setUrlInfo={setUrlInfo} />
+        <HistoryAccordion urlInfo={urlInfo}/>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
